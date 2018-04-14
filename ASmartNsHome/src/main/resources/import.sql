2140273f011f066d265b1f0895cade0e7b85c43e
yuekaizone <596760835@qq.com>
2018-04-14 16:24:28 +0800

    fix index add import.sql

---
+++ ASmartNsHome/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+
+insert into p1 (id, name, flag, title, description, content, position, text1, text2, text3, text4) values (1, '产品介绍', 'portfolio', '为赞而生', '卓越的技术，优质的产品，良好的服务，只要你有，你想展示，这里就需要你', '', '', '', '', '', '');
+
+insert into p1 (id, name, flag, title, description, content, position, text1, text2, text3, text4) values(2, '关于我们', 'about', '关于我们', '这是一个tests', '', '', '', '', '', '');
