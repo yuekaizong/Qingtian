5bb47f28123d57cc90adb2c83029e169aaba59df
yuekaizone <596760835@qq.com>
2018-04-14 20:40:41 +0800

    fix index.html

--- ASmartNsHome/src/main/java/kaizone/songmaya/xuanguan/controller/IndexController.java
+++ ASmartNsHome/src/main/java/kaizone/songmaya/xuanguan/controller/IndexController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -18,5 +19,11 @@ public class IndexController {
         model.addAttribute("name", "这是一个thymeleaf");
         model.addAttribute("data", divJPA.findAll());
         return "index";
+    }
+
+    @RequestMapping(value = "/data")
+    @ResponseBody
+    public Object getHomeData(Model model){
+        return divJPA.findAll();
     }
 }
--- ASmartNsHome/src/main/resources/import.sql
+++ ASmartNsHome/src/main/resources/import.sql
@@ -1,4 +1,12 @@
 
 insert into p1 (id, name, flag, title, description, content, position, text1, text2, text3, text4) values (1, '产品介绍', 'portfolio', '为赞而生', '卓越的技术，优质的产品，良好的服务，只要你有，你想展示，这里就需要你', '', '', '', '', '', '');
+insert into p1 (id, name, flag, title, description, content, position, text1, text2, text3, text4) values (2, '关于我们', 'about', '关于我们', '这是一个tests', '', '', '', '', '', '');
+insert into p1 (id, name, flag, title, description, content, position, text1, text2, text3, text4) values (3, '最新动态', 'news', '最新新闻', '这是一个醉醺醺的新闻', '', '', '', '', '', '');
+insert into p1 (id, name, flag, title, description, content, position, text1, text2, text3, text4) values (4, '商务服务', 'contact', '为你服务', '这是一个服务', '', '', '', '', '', '');
 
-insert into p1 (id, name, flag, title, description, content, position, text1, text2, text3, text4) values(2, '关于我们', 'about', '关于我们', '这是一个tests', '', '', '', '', '', '');
+insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(1,1,"images/demo/portfolio-1.jpg", '标题1', 'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
+insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(2,1,"images/demo/portfolio-2.jpg", '标题2', 'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
+insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(3,1,"images/demo/portfolio-3.jpg", '标题3',  'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
+insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(4,1,"images/demo/portfolio-4.jpg", '标题4',  'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
+insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(5,1,"images/demo/portfolio-5.jpg", '标题5',  'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
+insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(6,1,"images/demo/portfolio-6.jpg", '标题6', 'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
