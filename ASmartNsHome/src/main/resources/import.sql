7b0aafa7e0048df635881697f02e792442861d74
yuekaizone <596760835@qq.com>
2018-04-15 18:19:32 +0800

    完善index,

--- ASmartNsHome/src/main/resources/import.sql
+++ ASmartNsHome/src/main/resources/import.sql
@@ -1,8 +1,8 @@
 
 insert into p1 (id, name, flag, title, description, content, position, text1, text2, text3, text4) values (1, '产品介绍', 'portfolio', '为赞而生', '卓越的技术，优质的产品，良好的服务，只要你有，你想展示，这里就需要你', '', '', '', '', '', '');
-insert into p1 (id, name, flag, title, description, content, position, text1, text2, text3, text4) values (2, '关于我们', 'about', '关于我们', '这是一个tests', '', '', '', '', '', '');
+insert into p1 (id, name, flag, title, description, content, position, text1, text2, text3, text4) values (2, '关于我们', 'about', '关于我们', '这是一个tests', '', '', '设计 开发', '记录 展现', '现场 真实', '品质 标准');
 insert into p1 (id, name, flag, title, description, content, position, text1, text2, text3, text4) values (3, '最新动态', 'news', '最新新闻', '这是一个醉醺醺的新闻', '', '', '', '', '', '');
-insert into p1 (id, name, flag, title, description, content, position, text1, text2, text3, text4) values (4, '商务服务', 'contact', '为你服务', '这是一个服务', '', '', '', '', '', '');
+insert into p1 (id, name, flag, title, description, content, position, text1, text2, text3, text4) values (4, '帮助建议', 'help', '为你服务', '如需帮助和建议，请在下面填写你建议和问题，我们将为你提供继续完善，为你提供更好的解决方案', '', '', '', '', '', '');
 
 insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(1,1,"images/demo/portfolio-1.jpg", '标题1', 'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
 insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(2,1,"images/demo/portfolio-2.jpg", '标题2', 'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
@@ -10,3 +10,12 @@ insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,posi
 insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(4,1,"images/demo/portfolio-4.jpg", '标题4',  'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
 insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(5,1,"images/demo/portfolio-5.jpg", '标题5',  'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
 insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(6,1,"images/demo/portfolio-6.jpg", '标题6', 'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
+
+insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(7,2,"img", '技能', 'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','99%');
+insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(8,2,"img", '品质', 'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','99%');
+insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(9,2,"img", '服务', 'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','99%');
+insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(10,2,"img", '缺陷', 'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','0%');
+
+insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(11,3,"images/demo/author-2.jpg", '标题1', 'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
+insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(12,3,"images/demo/author-6.jpg", '标题2', 'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
+insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(13,3,"images/demo/author-4.jpg", '标题3', 'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
