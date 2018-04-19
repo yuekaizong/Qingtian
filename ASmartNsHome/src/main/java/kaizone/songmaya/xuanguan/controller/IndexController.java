58831158fbe4f8f2cd7df3a3f170ea20812b0bc9
yuekaizong <596760835@qq.com>
2018-04-19 12:24:20 +0800

    fix editHome add div name add ajax

--- ASmartNsHome/src/main/java/kaizone/songmaya/xuanguan/controller/IndexController.java
+++ ASmartNsHome/src/main/java/kaizone/songmaya/xuanguan/controller/IndexController.java
@@ -1,12 +1,17 @@
 package kaizone.songmaya.xuanguan.controller;
 
+import kaizone.songmaya.xuanguan.entity.DivEntity;
 import kaizone.songmaya.xuanguan.jpa.DivJPA;
 import kaizone.songmaya.xuanguan.jpa.FooterJPA;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -32,6 +37,13 @@ public class IndexController {
         model.addAttribute("data", divJPA.findAll());
         model.addAttribute("footer", footerJPA.findOne(1l));
         return "system/editHome";
+    }
+
+    @RequestMapping(value = "/editHome", method = RequestMethod.POST)
+    @ResponseBody
+    public String editHomeSave(@RequestBody List<DivEntity> model){
+        System.out.println(model.toString());
+        return "success";
     }
 
     @RequestMapping(value = "/data")
--- ASmartNsHome/src/main/resources/static/js/editHome.js
+++ ASmartNsHome/src/main/resources/static/js/editHome.js
@@ -1,22 +1,42 @@
+console.log("welcome editHome");
 
-  function getFileUrl(sourceId) {
-       var url;
-       if (navigator.userAgent.indexOf("MSIE")>=1) { // IE
-           url = document.getElementById(sourceId).value;
-       } else if(navigator.userAgent.indexOf("Firefox")>0) { // Firefox
-           url = window.URL.createObjectURL(document.getElementById(sourceId).files.item(0));
-       } else if(navigator.userAgent.indexOf("Chrome")>0) { // Chrome
-           url = window.URL.createObjectURL(document.getElementById(sourceId).files.item(0));
-       }
-       return url;
+function getFileUrl(sourceId) {
+   var url;
+   if (navigator.userAgent.indexOf("MSIE")>=1) { // IE
+       url = document.getElementById(sourceId).value;
+   } else if(navigator.userAgent.indexOf("Firefox")>0) { // Firefox
+       url = window.URL.createObjectURL(document.getElementById(sourceId).files.item(0));
+   } else if(navigator.userAgent.indexOf("Chrome")>0) { // Chrome
+       url = window.URL.createObjectURL(document.getElementById(sourceId).files.item(0));
    }
-   function previewImg(sourceId, targetId) {
-       var url = getFileUrl(sourceId);
-       var imgPre = document.getElementById(targetId);
-       imgPre.src = url;
+   return url;
+}
+
+function previewImg(sourceId, targetId) {
+   var url = getFileUrl(sourceId);
+   var imgPre = document.getElementById(targetId);
+   imgPre.src = url;
+}
+
+function previewText(sourceId, targetId) {
+   var dest = document.getElementById(targetId);
+   document.getElementById(targetId).innerHTML = dest.value;
+}
+
+function getData(){
+   $.ajax({
+     type: "GET",
+     url: "localhost/data",
+     success: success,
+     error: fail
+   });
+
+   function success(result){
+      console.log(result);
    }
 
-   function previewText(sourceId, targetId) {
-       var dest = document.getElementById(targetId);
-       document.getElementById(targetId).innerHTML = dest.value;
+   function fail(result){
+     console.log(result);
    }
+}
+
