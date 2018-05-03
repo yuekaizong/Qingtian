780cd4348ae21de9396cc575f1d8e7df65814f1c
yuelb <596760835@qq.com>
2018-05-03 19:24:10 +0800

    fix import.sql

--- ASmartNsHome/src/main/resources/import.sql
+++ ASmartNsHome/src/main/resources/import.sql
@@ -4,7 +4,7 @@ insert into p1 (id, name, flag, title, description, content, position, text1, te
 insert into p1 (id, name, flag, title, description, content, position, text1, text2, text3, text4) values (3, '最新动态', 'news', '最新新闻', '这是一个醉醺醺的新闻', '', '', '', '', '', '');
 insert into p1 (id, name, flag, title, description, content, position, text1, text2, text3, text4) values (4, '帮助建议', 'help', '为你服务', '如需帮助和建议，请在下面填写你建议和问题，我们将为你提供继续完善，为你提供更好的解决方案', '', '', '', '', '', '');
 
-insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(101,1,"images/demo/portfolio-1.jpg", '标题1', 'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
+insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(101,1,"images/demo/portfolio-1.jpg", '青', '取之于蓝，而青于蓝','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
 insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(102,1,"images/demo/portfolio-2.jpg", '标题2', 'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
 insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(103,1,"images/demo/portfolio-3.jpg", '标题3',  'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
 insert into p1_cell(id,pid,img,title,description,href,data_target,view_more,position,content,name,value) values(104,1,"images/demo/portfolio-4.jpg", '标题4',  'description','http://hao123.com','dataTarget','viewMore','x=120,y=100','内容','名','值');
