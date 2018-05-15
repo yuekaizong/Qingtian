904e3c56c2a9e971192e291ca9f2f85cd57ac4a7
yuelb <596760835@qq.com>
2018-05-15 17:13:22 +0800

    add 高可用分布式的配置中心

--- CloudConfigServer/src/main/java/kaizone/songmaya/qingtian/ConfigServerApp.java
+++ CloudConfigServer/src/main/java/kaizone/songmaya/qingtian/ConfigServerApp.java
@@ -3,9 +3,12 @@ package kaizone.songmaya.qingtian;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+//add 高可用分布式配置中心
+@EnableEurekaClient
 public class ConfigServerApp {
 
     public static void main(String[] args) {
