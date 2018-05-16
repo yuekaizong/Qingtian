a0c99a3348c90ac140f196d0cf7863d03b05f6ac
yuelb <596760835@qq.com>
2018-05-16 18:29:55 +0800

    update foo value test spring-cloud-bus

--- CloudConfigServer/src/main/java/kaizone/songmaya/qingtian/ConfigServerApp.java
+++ CloudConfigServer/src/main/java/kaizone/songmaya/qingtian/ConfigServerApp.java
@@ -2,13 +2,14 @@ package kaizone.songmaya.qingtian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
 //add 高可用分布式配置中心
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ConfigServerApp {
 
     public static void main(String[] args) {
